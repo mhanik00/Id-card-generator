8f9188f6c8f4796e0786afccad25ae440de872b8
mhanik00 <mhanik063@gmail.com>
2024-04-01 17:01:24 +0600

    add css and remove image input ,image input is not working now

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
-
-function generateID(){
+function generateID() {
     console.log("Generating id card");
 
-    const name = document.getElementById("name").value;
-    const fatherName = document.getElementById("fatherName").value;
-    const motherName = document.getElementById("motherName").value;
-    const Class = document.getElementById("class").value;
-    const roll = document.getElementById("roll").value;
-    const session = document.getElementById("session").value;
-    const section = document.getElementById("section").value;
-    const birthday = document.getElementById("birthday").value;
+    const Name = document.getElementById("name").value.trim();
+    name.value = "";
+
+    const FatherName = document.getElementById("fatherName").value.trim();
+    fatherName.value = "";
+
+    const MotherName = document.getElementById("motherName").value.trim();
+    motherName.value = "";
+
+    const Class = document.getElementById("ClassNumber").value;
+    ClassNumber.value = "";
+    
+    if (Class <= 0) {
+        alert("Enter valid class");
+        return;
+    }
+
+    const Roll = document.getElementById("roll").value;
+    roll.value = "";
+    if (roll <= 0) {
+        alert("Enter valid roll");
+        return;
+    }
+
+    const Session = document.getElementById("session").value;
+    session.value = "";
+    if (session <= 0) {
+        alert("Enter valid session");
+        return;
+    }
+
+    const Section = document.getElementById("section").value.trim();
+    section.value = "";
+
+    const Birthday = document.getElementById("birthday").value;
+    birthday.value = "";
+
     const gender = document.querySelector('input[name="gender"]:checked').value;
-    const issueDate=new Date().toLocaleDateString();
+    const issueDate = new Date().toLocaleDateString();
+
+    const inputs = document.querySelectorAll('input');
+
     // const image = document.getElementById("image").value;
 
-    
-
-    const Id= document.getElementById("idCard");
+    const Id = document.getElementById("idCard");
     Id.innerHTML = `
     <div class="generated-id-card">
-    <div class="generate-img"><div><h3>Momena Ali Biggan School</h3>Ullapara,Sirajgong <h5>Contact: 017450361744</h5></div>
+    <div class="generate-img"><div><h3>Your school Name</h3>Ullapara,Sirajgong <h5>Contact: 017450361744</h5></div>
     <img src="./images/th.jpeg" alt="image" class="generated-img"></div>
     <h4 style="text-align: center;">Id Card</h4>
     <div class=" generated-id-card-container">
-    
-
     <div class="generated-id-card-details">
-    <h4>Name :<span> ${name}</span></h4> 
-    <h4>Father Name :<span> ${fatherName}</span></h4> 
-    <h4>Mother Name :<span> ${motherName}</span></h4> 
-    <h4>Class :<span> ${Class}</span></h4> 
-    <h4>Date of Birth :<span> ${birthday}</span></h4> 
-    
-     </div>
+    <h4>Name :<span> ${Name}</span></h4>
+    <h4>Father Name :<span> ${FatherName}</span></h4>
+    <h4>Mother Name :<span> ${MotherName}</span></h4>
+    <h4>Class :<span> ${Class}</span></h4>
+    <h4>Date of Birth :<span> ${Birthday}</span></h4>
+    </div>
     <div>
-    <h4>Session :<span> ${session}</span></h4> 
-    <h4>Section :<span> ${section}</span></h4> 
-    <h4>Roll :<span> ${roll}</span></h4>
+    <h4>Session :<span> ${Session}</span></h4>
+    <h4>Section :<span> ${Section}</span></h4>
+    <h4>Roll :<span> ${Roll}</span></h4>
     <h4>Gender :<span> ${gender}</span></h4>
     <h4>Issue Date :<span> ${issueDate}</span></h4>
-    </div> 
-    
+    </div>
     </div>
     </div>
     `;
